b7d3283e7caf6dccd1f52e71015abb6bca954f9f
Fatima-ezzahra ELKAWKABI <fatizahra.elkawkabi@gmail.com>
2025-04-18 19:13:10 +0100

    user delete and update

--- back-office/backend/controllers/UserControllers.js
+++ back-office/backend/controllers/UserControllers.js
@@ -10,4 +10,55 @@ async function getUsers(req,res) {
   console.log("Error while getting users",error)
  }
 }
-module.exports={getUsers};
+
+async function editUser(req,res){
+ try{
+  const id = req.params.id;
+  const user = await User.findOne({where:{id:id}});
+  if(!user){
+   res.status(404).send("Utilisateur non trouvé");
+  }
+  res.status(200).send(user);
+ }
+ catch(error){
+  console.log("Erreur lors de l'edit de l'utilisateur",error);
+  res.status(200).send('Erreur au niveau du serveur');
+ }
+}
+
+async function updateUser(req,res){
+ try{
+  const id= req.params.id
+  const updatedData = req.body
+  const user = await User.findOne({where:{id:id}});
+  if(!user){
+   res.status(404).send('Utilisateur non trouvé');
+  }
+  await user.update(updatedData);
+  res.status(200).send('Utilisateur modifié avec succès');
+ }
+ catch(error){
+  console.log("Erreur lors de l'update de l'utilisateur",error);
+  res.status(200).send('Erreur au niveau du serveur');
+ }
+}
+
+async function deleteUser(req,res){
+ try{
+  const id = req.params.id
+  const user = await User.findOne({where:{id:id}})
+  if(!user){
+   return res.status(404).send("Utilisateur non trouvé");
+  }
+  await user.destroy();
+  res.status(200).send("Utilisateur bien supprimé");
+ }
+ catch(error){
+  console.log("Erreur lors de suppression de l'utilisateur",error);
+  res.status(500).send("Erreur au niveau du sérveur");
+ }
+}
+
+
+
+module.exports={getUsers,deleteUser,editUser,updateUser};
--- back-office/backend/routes/users.js
+++ back-office/backend/routes/users.js
@@ -1,6 +1,6 @@
 var express = require('express');
 const { register, login } = require('../controllers/authController');
-const {getUsers} = require("../controllers/UserControllers")
+const {getUsers, deleteUser, editUser, updateUser} = require("../controllers/UserControllers")
 var router = express.Router();
 
 /* GET users listing. */
@@ -8,4 +8,7 @@ var router = express.Router();
 router.post('/register',register);
 router.post('/login',login);
 router.get('/',getUsers);
+router.get('/edit/:id',editUser);
+router.put('/:id',updateUser);
+router.delete('/:id',deleteUser);
 module.exports = router;
